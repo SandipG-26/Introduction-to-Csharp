2ae6a2a7b7685f453d14a3bf8f09217ee5dcdce5
SandipG-26 <70084836+SandipG-26@users.noreply.github.com>
2020-08-30 14:19:27 +0530

    sandip's first

---
+++ Brainworks.Model/Student/Studentmodel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brainworks.Model.Student
+{
+    public class Studentmodel
+    {
+        public Studentmodel()
+        {
+
+        }
+        public Studentmodel(int id) : this()
+        {
+            Id = id + 1;
+        }
+        public Studentmodel(int id, string name) : this(id)
+        {
+            Name = name;
+        }
+        public int Id;
+        public string Name;
+        public List<Subjectmodel> subjects = new List<Subjectmodel>();
+        public void Introduce()
+        {
+            Console.WriteLine("Hello ! My name is " + Name + " and id is " + Id);
+        }
+
+    }
+}
---
+++ Introduction to Csharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using Brainworks.Model.Student;
+using System;
+
+
+namespace Brainworks
+{
+    public class program
+    {
+        static void Main()
+        {
+            Studentmodel studentFirst = new Studentmodel(1, "John");
+            studentFirst.Introduce();
+
+            Subjectmodel subject1 = new Subjectmodel();
+            subject1.Id = 1;
+            subject1.Name = "Computer Science";
+            studentFirst.subjects.Add(subject1);
+
+            Subjectmodel subject2 = new Subjectmodel();
+            subject2.Id = 2;
+            subject2.Name = " Science";
+            studentFirst.subjects.Add(subject2);
+
+            Subjectmodel subject3 = new Subjectmodel();
+            subject3.Id = 3;
+            subject3.Name = "Computer";
+            studentFirst.subjects.Add(subject3);
+            
+            foreach(Subjectmodel subject in studentFirst.subjects)
+            {
+                Console.WriteLine(subject.Name);
+            }
+             
+            Console.ReadKey();
+
+        }
+       
+    }
+    
+}
